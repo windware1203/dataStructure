0337b3ddd4e20fcf79db5d211f6b0298d0cbf66c
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2021-08-30 18:09:55 +0800

    fx:gui

--- summer2/homework2/src/sample/Main.java
+++ summer2/homework2/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("loader.fxml"));
 
         primaryStage.setTitle("Cheng Hao's Homework2");
         primaryStage.setScene(new Scene(root, 600, 600));
